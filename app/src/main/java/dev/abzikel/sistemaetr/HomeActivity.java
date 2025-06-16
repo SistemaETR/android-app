4869509fd8e9e3d1bb8ce06f0d71f0232442b130
abzikel <pikako0619@gmail.com>
2025-06-16 16:36:57 -0600

    Home screen created and sign out implemented

--- app/src/main/java/dev/abzikel/sistemaetr/HomeActivity.java
+++ app/src/main/java/dev/abzikel/sistemaetr/HomeActivity.java
@@ -1,9 +1,20 @@
 package dev.abzikel.sistemaetr;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,6 +22,63 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setup();
+    }
+
+    private void setup() {
+        // Link XML to Java
+        LinearLayout btnShortWeapon = findViewById(R.id.btnShortWeapon);
+        LinearLayout btnLongWeapon = findViewById(R.id.btnLongWeapon);
+        LinearLayout btnReaction = findViewById(R.id.btnReaction);
+        LinearLayout btnAdvancedReaction = findViewById(R.id.btnAdvancedReaction);
+        LinearLayout btnInfinite = findViewById(R.id.btnInfinite);
+        Button btnSignOut = findViewById(R.id.btnSignOut);
+
+        // Listeners
+        btnSignOut.setOnClickListener(v -> signOut());
+    }
+
+    private void signOut() {
+        // Inflate custom content dialog
+        Context context = this;
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View dialogView = inflater.inflate(R.layout.content_dialog_confirmation, null);
+
+        // Link XML to Java
+        TextView tvMessage = dialogView.findViewById(R.id.tvMessage);
+        Button btnPositive = dialogView.findViewById(R.id.btnPositive);
+        Button btnNegative = dialogView.findViewById(R.id.btnNegative);
+
+        // Initialize views
+        tvMessage.setText(getResources().getString(R.string.sign_out_confirmation));
+        btnPositive.setText(getResources().getString(R.string.sign_out));
+
+        // Create Alert Dialog to ask confirmation about logout
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.CustomAlertDialogStyle);
+        builder.setView(dialogView);
+        AlertDialog dialog = builder.create();
+
+        // Add listeners
+        btnPositive.setOnClickListener(v -> {
+            // Connect to Firebase Authentication and get user
+            FirebaseAuth auth = FirebaseAuth.getInstance();
+
+            // Sign out
+            auth.signOut();
+
+            // Return to login activity
+            Toast.makeText(context, context.getResources().getString(R.string.session_closed), Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(context, SignInActivity.class);
+            startActivity(intent);
+            finish();
+
+            // Dismiss dialog
+            if (dialog.isShowing()) dialog.dismiss();
+        });
+        btnNegative.setOnClickListener(v -> dialog.dismiss());
+
+        // Show Alert Dialog
+        dialog.show();
     }
 
 }
