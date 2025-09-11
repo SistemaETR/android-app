2e1627649a23b782419915dc9c381dd106903d68
abzikel <pikako0619@gmail.com>
2025-09-11 14:02:03 -0600

    Version changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "dev.abzikel.sistemaetr"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.0.0-beta.3"
+        versionCode = 4
+        versionName = "1.0.0-beta.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
