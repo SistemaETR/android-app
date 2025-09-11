5ac3c4bf836112a4f6fcb51b19a868fa159e549d
abzikel <pikako0619@gmail.com>
2025-09-11 12:56:46 -0600

    Native symbol upload enabled for Crashlytics

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+
+            firebaseCrashlytics {
+                nativeSymbolUploadEnabled = true
+            }
         }
     }
     compileOptions {
